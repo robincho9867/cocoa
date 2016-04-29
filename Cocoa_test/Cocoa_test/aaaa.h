fe476cb8cc6b6d57802804f34cd9a534a847b8a2
Robin <robin@sps-ltd.com>
2016-04-29 15:50:07 +0900

    aaaa

---
+++ Cocoa_test/Cocoa_test/aaaa.h
@@ -0,0 +1,13 @@
+//
+//  aaaa.h
+//  Cocoa_test
+//
+//  Created by Robin on 2016. 4. 29..
+//  Copyright © 2016년 Robin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface aaaa : ViewController
+
+@end
---
+++ Cocoa_test/Cocoa_test/aaaa.m
@@ -0,0 +1,13 @@
+//
+//  aaaa.m
+//  Cocoa_test
+//
+//  Created by Robin on 2016. 4. 29..
+//  Copyright © 2016년 Robin. All rights reserved.
+//
+
+#import "aaaa.h"
+
+@implementation aaaa
+
+@end
